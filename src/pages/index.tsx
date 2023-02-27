b8257464ccce39309238ea831d091f91671d40ab
RICARDO SOLIS <ricardosv46@gmail.com>
2023-02-26 21:56:49 -0500

    fix errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,12 @@ import Lading from "@components/Lading/Lading";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-    return (
-        <Container>
-            {/* <Lading /> */}
-            aqui estuvo jhoana zamora fababa
-        </Container>
-    );
+  return (
+    <Container>
+      <Lading />
+      {/* aqui estuvo jhoana zamora fababa */}
+    </Container>
+  );
 };
 
 export default Home;
